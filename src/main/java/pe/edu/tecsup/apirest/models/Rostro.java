eb443dbee7008b64716af662733e6eb70c37852b
Jose <jose.cristobal@tecsup.edu.pe>
2018-12-11 17:21:41 -0500

    corregido

--- src/main/java/pe/edu/tecsup/apirest/models/Rostro.java
+++ src/main/java/pe/edu/tecsup/apirest/models/Rostro.java
@@ -21,11 +21,11 @@ public class Rostro implements Serializable{
 	@NotNull(message="Este campo no puede ser nulo")
 	private String genero_rostro;
 	@NotNull(message="Este campo no puede ser nulo")
-	private String estado_rostro;	
+	private String estado_rostro;
 	@NotNull(message="Este campo no puede ser nulo")
 	private Long id_captura;
 	
-	public Long getId() {
+	public Long getId() {		
 		return id;
 	}
 	public void setId(Long id) {
@@ -37,21 +37,22 @@ public class Rostro implements Serializable{
 	public void setGenero_rostro(String genero_rostro) {
 		this.genero_rostro = genero_rostro;
 	}
-	public String getEstado_rostro() {
-		return estado_rostro;
-	}
-	public void setEstado(String estado_rostro) {
-		this.estado_rostro = estado_rostro;
-	}
 	public Long getId_captura() {
 		return id_captura;
 	}
 	public void setId_captura(Long id_captura) {
 		this.id_captura = id_captura;
 	}
+	
+	public String getEstado_rostro() {
+		return estado_rostro;
+	}
+	public void setEstado_rostro(String estado_rostro) {
+		this.estado_rostro = estado_rostro;
+	}
 	@Override
 	public String toString() {
-		return "Rostro [id=" + id + ", genero_rostro=" + genero_rostro + ", estado=" + estado_rostro + ", id_captura="
-				+ id_captura + "]";
+		return "Rostro [id=" + id + ", genero_rostro=" + genero_rostro + ", estado_rostro=" + estado_rostro
+				+ ", id_captura=" + id_captura + "]";
 	}
 }
